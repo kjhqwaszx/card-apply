8b847d317d0460a12e6a3d5b4ade300625c7671d
jaehan <kyobo.jaehan@gmail.com>
2025-05-30 17:43:35 +0900

    project init

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const CracoAlias = require('craco-alias')
+// font preload
+// const FontPreloadPlugin = require('webpack-font-preload-plugin')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+  // webpack: {
+  //   plugins: {
+  //     // font preload
+  //     add: [
+  //       new FontPreloadPlugin({
+  //         extensions: ['woff2'],
+  //       }),
+  //     ],
+  //   },
+  // },
+}
