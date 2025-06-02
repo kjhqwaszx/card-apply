6e647a8155d24950bf381d14abcc46f2b3b70e79
jaehan <kyobo.jaehan@gmail.com>
2025-06-02 14:01:24 +0900

    공통 컴포넌트 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,53 @@
 import React from 'react'
 import './App.css'
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
-import Text from '@shared/Text'
-
-const bold = css`
-  font-weight: bold;
-`
-
-const containerStyle = css`
-  background-color: pink;
-`
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
-  ${bold}
-`
+import Button from './components/shared/Button'
+import Input from '@shared/Input'
+import TextField from '@shared/TextField'
+import { useAlertContext } from '@/contexts/AlertContext'
 
 function App() {
+  const { open } = useAlertContext()
   return (
-    <div className="App" css={containerStyle}>
-      <Text typography="t1" display="block" color="red">
-        t1
-      </Text>
-      <Text typography="t2">t2</Text>
-      <Text typography="t3">t3</Text>
+    <div className="App">
+      <Button>클릭해주세요</Button>
+      <Button color="success">클릭해주세요</Button>
+      <Button color="error">클릭해주세요</Button>
+      <Button color="success" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button color="error" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button full={true}>클릭해주세요</Button>
+      <Button full={true} disabled={true}>
+        클릭해주세요
+      </Button>
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
+      <Input placeholder="로그인" aria-invalid={true} />
+      <Input />
+
+      <TextField label="아이디" />
+      <TextField label="패스워드" hasError={true} />
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드 신청 완료',
+            description: '카드 신청이 완료되었습니다.',
+            onButtonClick: () => {},
+          })
+        }}
+      >
+        {' '}
+        Alert open
+      </Button>
+
+      {/*<Alert*/}
+      {/*  open={true}*/}
+      {/*  title="제목"*/}
+      {/*  description="내용"*/}
+      {/*  onButtonClick={() => {}}*/}
+      {/*/>*/}
     </div>
   )
 }
--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,5 +1,15 @@
-import { ButtonColor, ButtonSize } from '@styles/button'
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
+import {
+  ButtonColor,
+  ButtonSize,
+  buttonColorMap,
+  buttonWeakMap,
+  buttonSizeMap,
+} from '@styles/button'
+// import Flex from './Flex'
+// import Text from './Text'
+// import Spacing from './Spacing'
 
 interface ButtonProps {
   color?: ButtonColor
@@ -9,9 +19,67 @@ interface ButtonProps {
   disabled?: boolean
 }
 
-const Button = styled.button<ButtonProps>({
-  // 디폴트 값
-  cursor: 'pointer',
-  fontWeight: 'bold',
-  borderRadius: '6px',
-})
+const Button = styled.button<ButtonProps>(
+  {
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    borderRadius: '6px',
+  },
+  ({ color = 'primary', weak }) =>
+    weak ? buttonWeakMap[color] : buttonColorMap[color],
+  ({ size = 'small' }) => buttonSizeMap[size],
+  ({ full }) =>
+    full
+      ? css`
+          display: block;
+          width: 100%;
+          border-radius: 0;
+        `
+      : undefined,
+  ({ disabled }) =>
+    disabled
+      ? css`
+          opacity: 0.26;
+          cursor: initial;
+        `
+      : undefined,
+)
+
+// function ButtonGroup({
+//   title,
+//   children,
+// }: {
+//   title?: string
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <Flex direction="column">
+//       {title != null ? (
+//         <>
+//           <Text typography="t6" bold={true}>
+//             {title}
+//           </Text>
+//           <Spacing size={8} />
+//         </>
+//       ) : null}
+//       <Flex css={buttonGroupStyle}>{children}</Flex>
+//     </Flex>
+//   )
+// }
+//
+// const buttonGroupStyle = css`
+//   flex-wrap: wrap;
+//   gap: 10px;
+//
+//   & button {
+//     flex: 1;
+//   }
+// `
+//
+// const Button = BaseButton as typeof BaseButton & {
+//   Group: typeof ButtonGroup
+// }
+//
+// Button.Group = ButtonGroup
+
+export default Button
