8379ed2b70d0f471aaf51207b3718a6929fc417a
김재한 <kyobo.jaehan@gmail.com>
2025-06-03 20:00:05 +0900

    카드상세 개발 & 애니메이션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '@pages/Home'
 import TestPage from '@pages/Test'
+import CardPage from '@pages/Card'
+import ScrollToTop from '@shared/SrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/card/:id" Component={CardPage} />
         <Route path="/test" element={<TestPage />} />
       </Routes>
     </BrowserRouter>
