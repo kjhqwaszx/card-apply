fcc910fa343f6bf3181d931d23f6b43e57022b24
김재한 <kyobo.jaehan@gmail.com>
2025-06-03 19:45:45 +0900

    CardList 개발

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,16 @@
 import Top from '@shared/Top'
-import { useEffect } from 'react'
-import getCards from '@remote/card'
-import { getAdBanners } from '@remote/adBanner'
+import AdBanners from '@components/home/AdBanners'
+import CardList from '@components/CardList'
 
 function HomePage() {
-  useEffect(() => {
-    getCards().then((result) => {
-      console.log(result)
-    })
-    getAdBanners().then((result) => {
-      console.log(result)
-    })
-  }, [])
-
   return (
     <div>
       <Top
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요 "
       />
+      <AdBanners />
+      <CardList />
     </div>
   )
 }
